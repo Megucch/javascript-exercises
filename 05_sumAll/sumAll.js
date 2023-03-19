4f6413c93d4ec1ddffaeabb94eb1eba90b792315
Megucch <120132522+Megucch@users.noreply.github.com>
2023-03-19 11:15:12 +0000

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    num1 < 0 ||
+    typeof num2 !== "number" ||
+    num2 < 0
+  )
+    return "ERROR";
+  let result = 0;
+  const values = [num1, num2];
+  values.sort((a, b) => a - b);
+  let sortNum1 = values[0];
+  let sortNum2 = values[1];
+  for (let i = sortNum1; i <= sortNum2; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
